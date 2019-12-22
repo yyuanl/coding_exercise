39d58b950b0dd7ead33116d3b71a00e19fd7c7f3
yyuanl <1092068432@qq.com>
2019-12-22 16:20:36 +0800

    update

---
+++ 0014_合并两个排序的链表/c++/main_recursion.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+struct ListNode{
+    public:
+        int val;
+        struct ListNode *next; 
+};
+ListNode* Merge(ListNode* pHead1, ListNode* pHead2){
+
+    if(pHead1 == NULL){
+        return pHead2;
+    }else if(pHead2 == NULL){
+        return pHead1;
+    }
+    
+    if(pHead1->val < pHead2->val){
+        pHead1->next = Merge(pHead1->next, pHead2);
+        return pHead1;
+    }else{
+        pHead2->next = Merge(pHead1, pHead2->next);
+        return pHead2;
+    }
+
+
+}
+
+
+
+void print_list(ListNode* pHead){
+    ListNode* p_temp = pHead;
+    while(p_temp != NULL){
+        cout<<p_temp -> val<<" "<<endl;
+        p_temp = p_temp -> next;
+    }
+}
+int main(){
+    
+    ListNode lianbiao_1[4];
+    lianbiao_1[0].val = 1;
+    lianbiao_1[0].next = &lianbiao_1[1];
+    lianbiao_1[1].val = 5;
+    lianbiao_1[1].next = &lianbiao_1[2];
+    lianbiao_1[2].val = 13;
+    lianbiao_1[2].next = &lianbiao_1[3];
+    lianbiao_1[3].val = 19;
+    lianbiao_1[3].next = NULL;
+
+    ListNode lianbiao_2[4];
+    lianbiao_2[0].val = 3;
+    lianbiao_2[0].next = &lianbiao_2[1];
+    lianbiao_2[1].val = 4;
+    lianbiao_2[1].next = &lianbiao_2[2];
+    lianbiao_2[2].val = 9;
+    lianbiao_2[2].next = &lianbiao_2[3];
+    lianbiao_2[3].val = 50;
+    lianbiao_2[3].next = NULL;
+
+    ListNode* list_temp = Merge(lianbiao_1, lianbiao_2);
+    print_list(list_temp);
+    return 0;
+}
