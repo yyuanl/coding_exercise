2b40d0370f0ed73a8a4a00cea767e4cf4d7a0ee8
yyuanl <1092068432@qq.com>
2019-12-26 00:00:19 +0800

    update

--- 0020_不用加减乘除做加法/c++/main.cpp
+++ 0020_不用加减乘除做加法/c++/main.cpp
@@ -1,10 +1,21 @@
 #include<iostream>
 using namespace std;
 int Add(int num1, int num2){
+    int temp;
+    int carray;
 
+    while(true){
+        temp = num1 ^ num2; //不带进位加法
+        carray = (num1 & num2) << 1;  // 进位
+        if(carray == 0)  // 进位为0推出
+            break;
+        num1 = temp;
+        num2 = carray;
+
+    }
+    return temp;
 }
 int main(){
-
-    
+    cout<<"12 + 56 = "<<Add(12, 56)<<endl;
     return 0;
 }
