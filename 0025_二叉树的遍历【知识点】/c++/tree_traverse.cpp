08d15087ebc648551edacee5078be3c1a13b41fb
yyuanl <1092068432@qq.com>
2019-12-30 00:46:33 +0800

    add

---
+++ 0025_二叉树的遍历【知识点】/c++/tree_traverse.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<string>
+using namespace std;
+struct TreeNode{
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x)
+        : val(x), left(NULL), right(NULL){}
+};
+void printNode(TreeNode &TN){ 
+    string left;
+    string right;
+    if(TN.left != NULL){
+        left = to_string(TN.left -> val);
+    }else{
+        left = "NULL";
+    }
+    if(TN.right != NULL){
+        right = to_string(TN.right -> val);
+    }else{
+        right = "NULL";
+    }
+    cout << "node "<<TN.val<<", left is "<<left<<", right is "<<right<<endl;
+}
+//非递归
+// 1. 先序
+void DLR(const TreeNode &TN){
+
+}
+
+
+int main(){
+    TreeNode TN1(1);
+    TreeNode TN2(2);
+    TreeNode TN3(3);
+    TreeNode TN4(4);
+    TreeNode TN5(5);
+    TreeNode TN6(6);
+    TreeNode TN7(7);
+    TreeNode TN8(8);
+    TreeNode TN9(9);
+    TreeNode TN10(10);
+    TreeNode TN11(11);
+    TreeNode TN12(12);
+    TN1.left = &TN2; TN1.right = &TN3;
+    TN2.left = &TN4; TN2.right = &TN5;
+    TN3.left = &TN6; TN3.right = &TN7;
+    TN4.left = NULL; TN4.right = NULL;
+    TN5.left = &TN8; TN5.right = &TN9;
+    TN6.left = NULL; TN6.right = &TN10;
+    TN7.left = &TN1; TN7.right = &TN12;
+    TN8.left = NULL; TN8.right = NULL;
+    TN9.left = NULL; TN9.right = NULL;
+    TN10.left = NULL; TN10.right = NULL;
+    TN11.left = NULL; TN11.right = NULL;
+    TN12.left = NULL; TN12.right = NULL;
+    printNode(TN1);
+
+    return 0;
+}
